43732e1602ed5424a678ee356bdefc9ec046f8e6
Владимир Алексеев <alekseevvladimir98@gmail.com>
2023-05-18 21:45:52 +0300

    minor stuff

--- game/GameCtrl.cpp
+++ game/GameCtrl.cpp
@@ -25,25 +25,10 @@ GameCtrl::GameCtrl() {
 	SystemsHolder::getInstance().addSystem<RotationSystem>();
 	SystemsHolder::getInstance().addSystem<ShadowMapGenerationSystem>();
 	RenderSystem* renderSystemPtr = SystemsHolder::getInstance().addSystem<RenderSystem>();
-	//Renderer test();
-	//std::unique_ptr<Renderer> test = std::make_unique<Renderer>();
 	renderSystemPtr->setRenderer(std::make_unique<Renderer>());
 
 
 	//createSpotLight({ false, false, "" });
 	//createBackPack({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f, 1.0f }, { true, false, "solidObject" });
-	/*
-	std::vector<std::vector<int>> levelData = readLevelData();
-	float brickWidth = RES_WIDTH / levelData[0].size();
-	auto& objectCtrl = GameObjectHolder::getInstance();
-	for (int i = 0; i < levelData.size(); i++) {
-		for (int j = 0; j < levelData[0].size(); j++) {
-			if (levelData[i][j] == 1) {
-				objectCtrl.createBrick(glm::vec3(j * brickWidth, i * 100.0f, 0.0f), { brickWidth, 100.0f, 1.0f }, { true, false });
-			}
-		}
-	}
-	objectCtrl.createPaddle(glm::vec3(200.0f, 550.0f, 1.0f), { 200.0f, 50.0f, 1.0f }, { true, false });
-	objectCtrl.createBall(glm::vec3(100.0f, -50.0f, 1.0f), { 50.0f, 50.0f, 1.0f }, { true, false });
-	*/
 }
+
