2a7a0f38ab6665aa40eccc45d598e6dca8516ac7
AlekseevVladimir <alekseevvladimir98@gmail.com>
2023-07-03 21:52:22 +0300

    added MSAA

--- engine/core/window_ctrl.cpp
+++ engine/core/window_ctrl.cpp
@@ -16,6 +16,8 @@ void WindowCtrl::init()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
 
 	m_window = glfwCreateWindow(RES_WIDTH, RES_HEIGHT, "First OpenGL app", NULL, NULL);
 	m_height = RES_HEIGHT;
@@ -37,6 +39,7 @@ void WindowCtrl::init()
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_STENCIL_TEST);
 	glEnable(GL_BLEND);
+	glEnable(GL_MULTISAMPLE);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glfwSetKeyCallback(m_window, keyCallback);
 	glfwSetCursorPosCallback(m_window, mouseCallback);
